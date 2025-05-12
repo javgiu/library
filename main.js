a4029f89824d21a808644b5e10e094ec9d5d946a
javgiu <javypicallo@gmail.com>
2025-05-12 15:57:56 -0400

    Add new features to add and delete books

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const book1 = new Book("Narnia Chronicles I", "C.S Lewis", crypto.randomUUID(), 
 const book2 = new Book("Narnia Chronicles II", "C.S Lewis", crypto.randomUUID(), true, 200);
 const book3 = new Book("Narnia Chronicles III", "C.S Lewis", crypto.randomUUID(), true, 153);
 
+// Dialog element 
+const dialogElement = document.querySelector("dialog");
+
+// Make the dialog easy to close  //
+// dialogElement.closedBy = "any";
+
+// Close dialog button
+const closeDialogButton = document.querySelector(".close-dialog");
+
+// Add dialog button 
+const addDialogButton = document.querySelector(".add");
+
 const myLibrary = [book1, book2, book3];
 
 function Book (title, author, id, read, pages) {
@@ -12,59 +24,110 @@ function Book (title, author, id, read, pages) {
     this.pages = pages;
 }
 
+const newBookButton = document.querySelector(".new-book");
+
+newBookButton.addEventListener("click", () => {
+    dialogElement.showModal();
+});
+
+const mainSection = document.querySelector("main");
+
+myLibrary.forEach(displayBook);
+
+function displayBook (book) {
+    const bookCard = document.createElement("div");
+    bookCard.className = "book-card";
+    bookCard.setAttribute("data-id", book.id);
+
+    const bookTitle = document.createElement("p");
+    bookTitle.innerText = book.title;
+    bookCard.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.innerText = book.author;
+    bookCard.appendChild(bookAuthor);
+
+    const bookPages = document.createElement("p");
+    bookPages.innerText = book.pages;
+    bookCard.appendChild(bookPages);
+
+    const bookState = document.createElement("p");
+    const bookSpanState = document.createElement("span");
+    if(book.read) {
+        bookSpanState.innerText = "Readed";
+    } else {
+        bookSpanState.innerText = "Unreaded";
+    }
+    bookState.appendChild(bookSpanState);
+    bookCard.appendChild(bookState);
+
+    const bookButtonsDiv = document.createElement("div");
+    const bookButtonDelete = document.createElement("button"); 
+    const bookButtonRead = document.createElement("button");
+
+    bookButtonsDiv.className = "buttons"
+
+    bookButtonDelete.innerText = "Delete";
+    bookButtonDelete.className = "delete";
+
+    bookButtonRead.innerText = bookSpanState.innerText;
+    bookButtonRead.className = "readed";
+    
+    bookButtonsDiv.appendChild(bookButtonDelete);
+    bookButtonsDiv.appendChild(bookButtonRead);
+    
+
+    bookCard.appendChild(bookButtonsDiv);
+
+
+    mainSection.appendChild(bookCard);
+};
+
+closeDialogButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialogElement.close();
+});
+
+addDialogButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    addBook();
+    dialogElement.close();
+});
+
 function addBook() {
 
-    const title = prompt("Title: "); 
-    const author = prompt("Author: "); 
+    const title = document.querySelector("#title").value; 
+    const author = document.querySelector("#author").value; 
     const id = crypto.randomUUID();
-    const read = true;
-    const pages = +(prompt("Number of pages: "));
+    const read = 
+        document.querySelector("#status").value == "yes" ? true : false;
+    const pages = document.querySelector("#pages").value;
 
     const newBook = new Book(title, author, id, read, pages);
+
+    displayBook(newBook);
 
     myLibrary.push(newBook);
 
     console.log(myLibrary);
+
+    console.dir(dialogElement);
 }
 
-const addBookButton = document.querySelector(".add");
+mainSection.addEventListener("click", deleteBook);
 
-addBookButton.addEventListener("click", addBook)
+function deleteBook(e) {
+    if (e.target.className === "delete") {
 
-const mainSection = document.querySelector("main");
+        const divToDelete = e.target.parentNode.parentNode;
+        console.dir(divToDelete);
+        const bookToRemoveIndex = myLibrary.findIndex(book => book.id === divToDelete.dataset.id);
 
-function displayBook () {
-    myLibrary.forEach((book) => {
-        const bookCard = document.createElement("div");
-        bookCard.className = "book-card";
-        bookCard.setAttribute("data-id", book.id);
+        console.log(bookToRemoveIndex);
 
-        const bookTitle = document.createElement("p");
-        bookTitle.innerText = book.title;
-        bookCard.appendChild(bookTitle);
+        myLibrary.splice(bookToRemoveIndex, 1);
 
-        const bookAuthor = document.createElement("p");
-        bookAuthor.innerText = book.author;
-        bookCard.appendChild(bookAuthor);
+        mainSection.removeChild(divToDelete);
+    }
 
-        const bookPages = document.createElement("p");
-        bookPages.innerText = book.pages;
-        bookCard.appendChild(bookPages);
-
-        const bookState = document.createElement("p");
-        const bookSpanState = document.createElement("span");
-        if(book.read) {
-            bookSpanState.innerText = "Readed";
-        } else {
-            bookSpanState.innerText = "Unreaded";
-        }
-        bookState.appendChild(bookSpanState);
-        bookCard.appendChild(bookState);
-
-        mainSection.appendChild(bookCard);
-    });
 }
-
-displayBook();
-
-console.log(myLibrary[1]);
